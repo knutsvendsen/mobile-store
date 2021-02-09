eea4d937251abddea2cd5add780a6e16ca1fdfe6
KnutSvendsen <knutsvendsen@mac.com>
2021-02-09 12:27:07 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/vivi_mobile_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vivi_mobile_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Product {
+  @required
+  final String id;
+  @required
+  final String title;
+  @required
+  final String description;
+  @required
+  final double price;
+  @required
+  final String imageUrl;
+  bool isFavorite = false;
+
+  Product({
+    this.id,
+    this.title,
+    this.description,
+    this.price,
+    this.imageUrl,
+    this.isFavorite = false,
+  });
+}
